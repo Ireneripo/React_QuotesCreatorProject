b6ab19d5291a0ab2ba2801c5e1a5281438ba6eea
Irene Riportella <riportellairene@gmail.com>
2022-10-27 20:49:53 +0200

    added main routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes';
+import NewQuote from './pages/NewQuote';
+import QuoteDetail from './pages/QuoteDetail';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route
+        path="/"
+        exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route
+        path="/quotes"
+        exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
 export default App;
+
+/*  
+Main Routes:
+- Page which shows all quotes
+- Quote detail page
+- New quote page
+*/
