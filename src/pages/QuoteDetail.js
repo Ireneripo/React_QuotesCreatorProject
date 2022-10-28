01c27cce4a306d817ce00247c68802079e3a829d
Irene Riportella <riportellairene@gmail.com>
2022-10-28 19:26:11 +0200

    modified changeSortingHandler function

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
+
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
 
@@ -11,7 +12,10 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
+
+  console.log(match);
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -25,7 +29,18 @@ const QuoteDetail = () => {
         text={quote.text}
         author={quote.author}
       />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route
+        path={match.path}
+        exact>
+        <div className="centered">
+          <Link
+            className="btn--flat"
+            to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
